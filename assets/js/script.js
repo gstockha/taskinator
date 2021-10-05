35ec70007aa329770b708364b60c1842e93606db
George Stockhammer <sunchipzzz@gmail.com>
2021-10-05 15:47:27 -0500

    spruced!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,41 @@
 var formEl = document.querySelector("#task-form"); //for button
 var tasksToDoEl = document.querySelector("#tasks-to-do"); //target area of html (via class)
 
-var createTaskHandler = function() {
+var taskFormHandler = function() {
     event.preventDefault(); //prevent refresh
-
-    var listItemEl = document.createElement("li"); //type of html element
-    listItemEl.className = "task-item"; //assign ID
-    listItemEl.textContent = "This is a new task."; //asign text to html
-    tasksToDoEl.appendChild(listItemEl); //add to tasks-to-do list
+    //we used this to see that 'value' was the relevant thing for string input
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    //we use this to see what type user selected on the <select> drop down
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    // check if input values are empty strings
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false; //get out of here
+    }
+    formEl.reset(); //seems non-important? but ok
+    //send it as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+    // send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
 }
 
-formEl.addEventListener("submit", createTaskHandler); //submit uses enter and click
+var createTaskEl = function(taskDataObj) {
+    // create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
+}
+
+formEl.addEventListener("submit", taskFormHandler); //submit uses enter and click
